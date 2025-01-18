ad4ab85ebcf562af83c5cae8bfbcbb035ef49646
sukiboo <dereventsov@gmail.com>
2025-01-18 11:16:14 -0500

    define server at top level

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import os
 from src.dash_app import create_app
 from src.prices import get_historical_prices
 
-if __name__ == "__main__":
-    tickers = ["AAPL", "MSFT", "GOOGL"]
-    df = get_historical_prices(tickers)
+tickers = ["AAPL", "MSFT", "GOOGL"]
+df = get_historical_prices(tickers)
 
-    dash_app, server = create_app(df)
+dash_app, server = create_app(df)
+
+if __name__ == "__main__":
     dash_app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 7860)))
